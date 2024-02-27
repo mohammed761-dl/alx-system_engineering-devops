f8f7397f335b10cc5890d6fc139d18d0fc2ca775
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2024-02-27 08:59:52 +0100

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Crate a file in /tmp
-file { 'school':
-  ensure  => present,
-  path    => '/tmp/school',
-  content => 'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# Kills a process named killmenow
-exec { 'kill':
-  command => 'pkill -f killmenow',
-  path    => ['/bin', '/usr/bin', '/usr/local/bin']
-}
